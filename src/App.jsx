1b4bc6c1121ef8488722bd20e07b60b344a7b999
ahmedashrafdiv <ahmedashrafngxx@gmail.com>
2024-09-11 00:19:01 +0300

    add files

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import  "/src/App.css"
+import Login from './components/Login/Login'
+
+function App() {
+
+
+  return (
+    <>
+      <div className='container2' >
+        <Login/>
+      </div>
+    </>
+  )
+} 
+
+export default App
---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,92 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useState } from "react";
+
+export default function Login() {
+  const [user, setUser] = useState({
+    name: "",
+    phone: "",
+    email: "",
+    password: "",
+  });
+
+  const heandelChange = (e) => {
+    const event = e.target;
+    setUser({ ...user, [event.name]: event.value });
+    console.log(user);
+  };
+
+  const isFormValid = () => {
+    return user.name && user.phone && user.email && user.password;
+  };
+
+  return (
+    <>
+      <form>
+        {/* name */}
+        <div className="mb-3">
+          <label htmlFor="name" className="form-label">
+            Name
+          </label>
+          <input
+            name="name"
+            type="text"
+            className="form-control"
+            id="name"
+            onChange={heandelChange}
+          />
+        </div>
+
+        {/* Phone */}
+        <div className="mb-3">
+          <label htmlFor="Phone" className="form-label">
+            Phone number
+          </label>
+          <input
+            name="phone"
+            type="number"
+            className="form-control"
+            onChange={heandelChange}
+            id="Phone"
+          />
+        </div>
+
+        {/* Email */}
+        <div className="mb-3">
+          <label htmlFor="exampleInputEmail1" className="form-label">
+            Email address
+          </label>
+          <input
+            name="email"
+            type="email"
+            className="form-control"
+            id="exampleInputEmail1"
+            onChange={heandelChange}
+            aria-describedby="emailHelp"
+          />
+        </div>
+
+        {/* Password */}
+        <div className="mb-3">
+          <label htmlFor="exampleInputPassword1" className="form-label">
+            Password
+          </label>
+          <input
+            name="password"
+            type="password"
+            className="form-control"
+            onChange={heandelChange}
+            id="exampleInputPassword1"
+          />
+        </div>
+
+        <button
+          type="submit"
+          className="btn btn-primary"
+          disabled={!isFormValid()}
+        >
+          Submit
+        </button>
+      </form>
+    </>
+  );
+}
